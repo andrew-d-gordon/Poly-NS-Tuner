6e1cebbddc8d507728b061bf936adb4dc398077a
andgordo <andgordo@ucsc.edu>
2021-03-18 08:39:14 -0700

    Added candidate peak weight calculation funcs to poly_note_detection.py

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ from scale_detection import *
 from audio_stream_test import decibelScale
 
 # CONSTANTS
-num_pitches = 1
+num_pitches = 3
 num_candidates = 20
 
 # LOAD SAMPLE/PREP BUFFER
-data, sr = load('samples/piano_chords_Cm_vanilla.wav')
+data, sr = load('samples/piano_chords_melody_Cm_vanilla.wav')
 
 # COMPUTE FOURIER TRANSFORM (VIA DFT)
 ft, xf, audio_len = compute_ft(data, sr)
@@ -22,93 +22,26 @@ ft_amp = convert_magnitude(ft, audio_len)
 current_peaks, current_peaks_freqs, current_peaks_amps = collect_peaks(ft_amp, xf, audio_len, num_candidates)
 current_peaks_midi_pitch = [note_to_midi_pitch(elem[0], elem[1]) for elem in current_peaks]  # create list w/midi pitch
 
-#PRINT CURRENT CANDIDATE PEAKS (FREQS AND KEYS)
-print(current_peaks)
-print(current_peaks_midi_pitch)
-print(current_peaks_freqs)
-print(current_peaks_amps)
+#PRINT CURRENT CANDIDATE PEAKS (NOTES, MIDI PITCHES, FREQS, AMPS)
+print("\nHere are feature lists for unique candidate peaks...\n")
+print("Current peaks notes:", current_peaks)
+print("Current peaks midi pitches:", current_peaks_midi_pitch)
+print("Current peaks freqs:", current_peaks_freqs)
+print("Current peaks amplitudes:", current_peaks_amps)
 
-# CANDIDATE PEAK LIKELIHOOD AND PITCH SELECTION (magnitude, harmonics, duration?)
+# CANDIDATE PEAK LIKELIHOOD CALCULATION AND FUNDAMENTAL SELECTION
+current_peak_weights = compute_peak_likelihood(current_peaks_midi_pitch, current_peaks_amps, num_candidates)
+print("\nCurrent peaks weights:", current_peak_weights)
 
-# DURATION/END AND START NOTE MONITORING?
+fundamental_predictions = retrieve_n_best_fundamentals(current_peak_weights, current_peaks, num_pitches)
+print("Current fundamental predictions:", fundamental_predictions)
+
+# DURATION/END AND START NOTE MONITORING
+
+#  ================================================= N/A
 
 # SCALE DETECTION (current_peaks_midi_pitch is input)
 n = noteSet()
 n.setNoteAmounts(current_peaks_midi_pitch)
 n.findClosestScale()
 print(n.closestScale)
-
-# PSEUDOCODE/NOTES
-'''
-L(f) is a non-negative likelihood function where f is frequency. The presence of peaks at or near multiples of f increases
-L(f) in a way which depends on the peak's amplitude and frequency as shown:
-
-L(f) = Summation(i=0,k){ai*ti*ni}
-Where k is number of peaks in the spectrum, 
--ai is a factor depending on the amplitude of the ith peak,
--ti depends on how closely the ith peak is tuned to a multiple of fi
--ni depends on whether the peak is closest to a low or high multiple of f
-
-For monophonic pitch estimation, we simply output the value of f whose "likelihood" is highest.
-For polyphonic pitch estimation, we successively take the values of f of greatest likelihood which are neither multiples
-nor sub-multiples of a previous one.  (loosen up on being sub multiple for octaves)
-
-In all cases, last criteria to determine if there is pitch (as L(f) will always have a maximum even if no pitch).
-Our criterion is that there either be at least four peaks present or else that the fundamental be present and the total 
-power of contributing peaks be at least a hundredth of the signal power.
-'''
-
-# 5 peaks glocka2:
-
-# [('A6', 1759.4416562107906),
-# # ('D8', 4757.277289836888),
-# # ('B8', 8031.116687578419),
-# # ('D#9', 9788.58218318695),
-# # ('A#6', 1812.1392722710164)]
-
-'''
-Notes/Pseudo:
-Algo originally uses optimized discrete hartley transform, any reasonably fast dft algo should be fine.
-Rob Mayer wrote traditionally used code for this algorithm, denoted "Fastest Fourier Transform in the West"
-
-1) Fiddle~ computes a DFT of a block of input, zeropadded by a factor of four, three main steps:
-    1.1) Initial Computation,
-    1.2) Spectral interpolation, 
-    1.3) and frequency domain windowing
-
-1.1) Initial Computation
-Suppose you have a real, length N signal x[n], and we modulate signal by complex exponential e^-j*(pi/2N)*n to obtain:
-xmod[n] = x[n]*e^-j*(pi/2N)*n
-
-We can compute DFT of this modulated signal as:
-
-Xmod[k] = Summation(n=0, N-1){x[n]*(e^-j*(pi/2N)*n)*(e^-j*(2pi/N)*kn)} = Summation(n=0, N-1){x[n]*(e^-j*(pi/2N)*(4k+1)n)}
-for k E [0, N-1].
-
-Now suppose we take same signal x[n], and rather than modulate it, we zeropad it to length 4N to obtain a signal xzeropad[n].
-DFT of this new signal is...
-Xzeropad[k]=Summation(n=0, N-1){x[n]*(e^-j*(2pi/4N)*kn)}=Summation(n=0, N-1){x[n]*(e^-j*(pi/2N)*kn)}
-for k E [0, 4N-1]. Note that...
-
-Xmod[k] = Xzeropad[4k+1]
-
-Guessing Fundamental Frequencies...
-
-L(f) is a non-negative likelihood function where f is frequency. The presence of peaks at or near multiples of f increases
-L(f) in a way which depends on the peak's amplitude and frequency as shown:
-
-L(f) = Summation(i=0,k){ai*ti*ni}
-Where k is number of peaks in the spectrum, 
--ai is a factor depending on the amplitude of the ith peak,
--ti depends on how closely the ith peak is tuned to a multiple of fi
--ni depends on whether the peak is closest to a low or high multiple of f
-
-For monophonic pitch estimation, we simply output the value of f whose "likelihood" is highest.
-For polyphonic pitch estimation, we successively take the values of f of greatest likelihood which are neither multiples
-nor sub-multiples of a previous one.  (loosen up on being sub multiple for octaves)
-
-In all cases, last criteria to determine if there is pitch (as L(f) will always have a maximum even if no pitch).
-Our criterion is that there either be at least four peaks present or else that the fundamental be present and the total 
-power of contributing peaks be at least a hundredth of the signal power.
-
-'''
--- src/poly_note_detection.py
+++ src/poly_note_detection.py
@@ -7,10 +7,19 @@ from freq_note_conversions import freq_to_note
 # from scipy.io import wavfile as wav
 import heapq
 
+# CONSTANTS
 
-def maxInFT(yf, xf, audio_len):
+# Harmonic Series based on, e.g. C3, overtones = C4(+12), G4(+19), C5(+24), and E5(+28)...
+harmonic_series_mp = [12, 19, 24, 28, 31]
+harmonic_series_weight = [2, 1.8, 1.6, 1.4, 1.2]
+
+# Formant weights/mp intervals, soon...
+
+# FUNCTIONS
+
+def max_in_ft(yf, xf, audio_len):
     max_mag_idx = np.argmax(yf[:audio_len // 2], axis=0)  # get max idx
-    #print(maxMagIdx, " ", yf[maxMagIdx], " ", xf[maxMagIdx])
+    # print(maxMagIdx, " ", yf[maxMagIdx], " ", xf[maxMagIdx])
     max_amp = yf[max_mag_idx]
     yf[max_mag_idx] = 0
     return xf[max_mag_idx], max_amp
@@ -27,27 +36,27 @@ def fftplot(yf, xf, audio_len):
 
 def compute_ft(audio, sr):
     audio_len = len(audio)
+    window = blackman(audio_len)  # optional windowing to aide spectral leakage
+    yf = fft(audio * window)
     T = 1 / sr
-    window=blackman(audio_len)  # optional windowing to aide spectral leakage
-    yf = fft(audio*window)
     xf = np.linspace(0.0, 1.0 / (2.0 * T), audio_len // 2)
-    #fftplot(yf, xf, audio_len) #optional plot
+    fftplot(yf, xf, audio_len) #optional plot
     return yf, xf, audio_len
 
 
 def convert_magnitude(yf, audio_len):
-    yfconvert = [math.sqrt(elem.real**2 + elem.imag**2) for elem in yf]
+    yfconvert = [math.sqrt(elem.real ** 2 + elem.imag ** 2) for elem in yf]
     return yfconvert
 
 
 def collect_peaks(yf, xf, audio_len, num_candidates):
     current_peaks = []  # list for peak notes and scales
-    current_peaks_freq=[] # list for peak frequencies
-    current_peaks_amps=[] # list for peak amplitudes
+    current_peaks_freq = []  # list for peak frequencies
+    current_peaks_amps = []  # list for peak amplitudes
 
     while len(current_peaks) < num_candidates:
         # grab maximum magnitude and frequency, magnitude for frequency set to 0, initial mag returned to candidate_mag
-        max_mag_freq, candidate_mag = maxInFT(yf, xf, audio_len)
+        max_mag_freq, candidate_mag = max_in_ft(yf, xf, audio_len)
 
         # translate to key (candidate)
         candidate_peak_ns = freq_to_note(max_mag_freq)
@@ -57,13 +66,67 @@ def collect_peaks(yf, xf, audio_len, num_candidates):
             current_peaks.append(candidate_peak_ns)
             current_peaks_freq.append(max_mag_freq)
             current_peaks_amps.append(candidate_mag)
-            print("Candidate Peak, Candidate Freq, Candidate Magnitude (amp):", candidate_peak_ns, "|", max_mag_freq, "|", candidate_mag)
+            print("Candidate Peak Note, Freq, Magnitude (amp):", candidate_peak_ns, "|", max_mag_freq,
+                  "|", candidate_mag)
 
     return current_peaks, current_peaks_freq, current_peaks_amps
 
 
-def compute_peak_likelihood(peak_notes, peak_freqs, peak_mags):
-    print("computing likelihood")
+def retrieve_n_best_fundamentals(current_peak_weights, current_peaks, num_pitches):
+    fundamental_predictions = []
+    pitches_selected = 0
+    while pitches_selected < num_pitches:
+        # Select largest weight, find idx and corresponding note, append to prediction list
+        best_f0_idx = np.argmax(current_peak_weights)
+        fundamental_predictions.append((current_peaks[best_f0_idx], current_peak_weights[best_f0_idx]))
+
+        # Void value at most recent max, continue search for f0's until num_pitches f0's have been found
+        current_peak_weights[best_f0_idx] = 0
+        pitches_selected += 1
+
+    return fundamental_predictions
+
+
+def peak_to_f_harmonic_weight(f_mp, p_mp):  # Provides value of ti*ni
+    if p_mp < f_mp:  # Base Case, returns if peak < f0
+        return 1
+
+    for idx in range(len(harmonic_series_mp)):
+        if f_mp+harmonic_series_mp[idx] == p_mp:
+            return harmonic_series_weight[idx] # peak is a multiple of F0
+
+    # Peak is not a multiple of f0
+    return 1
+
+
+'''
+L(f) = Summation(i=0,k){ai*ti*ni}
+Where k is number of peaks in the spectrum,
+-ai is a factor depending on the amplitude of the ith peak,
+-ti depends on how closely the ith peak is tuned to a multiple of fi
+-ni depends on whether the peak is closest to a low or high multiple of f
+'''
+
+
+def compute_peak_likelihood(current_peaks_mp, current_peaks_amps, num_candidates):
+    current_peak_weights = []
+
+    for f in range(num_candidates):
+        f_weight = 0
+        for i in range(num_candidates):
+            peak_weight = 0  # ai*ni*ti
+            if i != f:  # skip fundamental whose likelihood is being calculated
+                peak_weight += current_peaks_amps[i]  # ai
+                peak_weight *= peak_to_f_harmonic_weight(current_peaks_mp[f], current_peaks_mp[i])  # ni*ti
+                f_weight += peak_weight
+            else:
+                continue
+
+        #print("This is weight for f:", current_peaks_mp[f], "->", f_weight)
+        current_peak_weights.append(f_weight)
+
+    return current_peak_weights
+
     '''
     L(f) = Summation(i=0,k){ai*ti*ni}
     Where k is number of peaks in the spectrum, 
@@ -80,11 +143,9 @@ def compute_peak_likelihood(peak_notes, peak_freqs, peak_mags):
     power of contributing peaks be at least a hundredth of the signal power.
     '''
 
-    #pieces:
-    #1)amplitude decision, amp or db? what is threshold?
-    #2)determine if self is harmonic overtone, if at least half db of "fundamental" do not dock weight
-    #3)determine value of each overtone, if first octave *2, if first octave fifth *1.5, second octave *1, second octave third *.5
-    #4)set exit decision criteria, how harsh to dock multiples of other frequencies, amplitude threshold, ...
-    #overtone series: e.g. C3 then the overtones C4, G4, C5, and E5
-
-    '''[('G', 4), ('D#', 4), ('D', 4), ('D', 5), ('G#', 4), ('F', 4), ('F', 5), ('D#', 5), ('E', 4), ('G#', 5)]'''
+    # pieces:
+    # 1)amplitude decision, amp or db? what is threshold?
+    # 2)determine if self is harmonic overtone, if at least half db of "fundamental" do not dock weight
+    # 3)determine value of each overtone, if first octave *2, if first octave fifth *1.5, second octave *1, second octave third *.5
+    # 4)set exit decision criteria, how harsh to dock multiples of other frequencies, amplitude threshold, ...
+    # overtone series: e.g. C3 then the overtones C4, G4, C5, and E5
